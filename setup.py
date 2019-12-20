893c794044acf202768efa9bc0c407d0b3435ebe
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2019-12-20 14:37:51 +0900

    refine package

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
 import os,sys
 from setuptools import setup,find_packages
 
-def read_requirements():
-    """Parse requirements from requirements.txt."""
-    reqs_path = os.path.join('.', 'requirements.txt')
-    with open(reqs_path, 'r') as f:
-        requirements = [line.rstrip() for line in f]
-    return requirements
-
 setup(
     name="IndianPines",
-    version="0.0.1",
+    version="0.0.2",
     install_requires=["scikit-learn", "numpy","pandas"],
-    #description='Sample package for Python-Guide.org',
-    #long_description=readme,
+    description='Indian Pines datasets for scikit-learn',
+    long_description=Readme.md,
     author='Kotaro Sonoda',
     author_email='kotaro1976@gmail.com',
-    #install_requires=read_requirements(),
     url='https://github.com/helmenov/IndianPines',
-    #license=license,
+    license=MIT,
     packages=find_packages(),
     include_package_data=True,
 )
